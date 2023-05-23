a3d8c027a3db2d7bb932100782656bb42e327b7d
ki052020 <56394712+ki052020@users.noreply.github.com>
2023-05-23 16:17:59 +0900

    Update n0001.js

--- n0001.js
+++ n0001.js
@@ -1,5 +1,5 @@
 const txts = [
-  "I rarely eat snacks and saw this at my local store.",
+  "Ioooo rarely eat snacks and saw this at my local store.",
   "I love spicy snacks but didn't really care for Cheetos but I wanted to try it out to see how hot it is.",
 ];
 gWorkBook.SetTxts(txts);
