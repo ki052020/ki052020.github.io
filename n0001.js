3a76ba3b856b95624b7db64360834f6297d0d895
ki052020 <56394712+ki052020@users.noreply.github.com>
2023-05-29 18:19:09 +0900

    Update n0001.js

--- n0001.js
+++ n0001.js
@@ -1,5 +1,9 @@
 const txts = [
-  "Ioooo rarely eat snacks and saw this at my local store.",
+  "I rarely eat snacks and saw this at my local store.",
   "I love spicy snacks but didn't really care for Cheetos but I wanted to try it out to see how hot it is.",
+  "This is by far the hottest snacks from a normal food I've ever had.",
+  "As a spice head It's alright, but if you're someone who's not used to eating spicy foods, you're going to burn your mouth.",
+  "It's pretty damn hot.",
 ];
-gWorkBook.SetTxts(txts);
+
+console.log("loading -> n0001.js");
